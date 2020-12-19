b4848d6c0b2ded2a7702ccb2755b1a840191a20f
XxArnavxX <60574509+XxArnavxX@users.noreply.github.com>
2020-12-19 22:49:16 +0530

    Add files via upload

---
+++ bank.py
@@ -0,0 +1,11 @@
+class Atm(object):
+    def __init__(self, name, ccv, cardNumber):
+        self.name = name
+        self.ccv = ccv
+        self.cardNumber = cardNumber
+    def Pay(self, keytype):
+        print("Withdrawl: " + Pay)
+
+atm1 = Atm("bob", "782", "1082099")
+print(atm1.name, atm1.ccv, atm1.cardNumber)
+atm1.Pay("success")
